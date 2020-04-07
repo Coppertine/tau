ad64d7ca53d98be67c6d8f360d81200393880457
Coppertine <37494321+Coppertine@users.noreply.github.com>
2020-04-07 17:40:28 +1000

    Start on decoder and beatmap processor

--- osu.Game.Rulesets.tau/Beatmaps/Formats/TauLegacyBeatmapEncoder.cs
+++ osu.Game.Rulesets.tau/Beatmaps/Formats/TauLegacyBeatmapEncoder.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using osu.Game.Beatmaps;
 
 namespace osu.Game.Rulesets.Tau.Beatmaps.Formats
 {
@@ -11,5 +12,10 @@ namespace osu.Game.Rulesets.Tau.Beatmaps.Formats
         /// HardBeat = 4,
         /// Slider = 8,
         /// HardSlider = HardBeat | Slider // = 12
+
+        public const int LATEST_VERSION = 1;
+
+        private readonly IBeatmap beatmap;
+
     }
 }
---
+++ osu.Game.Rulesets.tau/Beatmaps/TauBeatmapProcessor.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using osu.Game.Beatmaps;
+
+namespace osu.Game.Rulesets.Tau.Beatmaps
+{
+    public class TauBeatmapProcessor : BeatmapProcessor
+    {
+    }
+}
